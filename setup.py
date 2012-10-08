9a7a6dde2f5ce945ef5b287b3b1143c20da7bc98
Simon Guest <simon.guest@tesujimath.org>
2012-10-09 12:11:23 +1300

    Updated setup.py for 0.2

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@
 from distutils.core import setup
 
 setup(name='qlsync',
-      version='0.1',
+      version='0.2',
       description='Sync utility for Quodlibet',
       author='Simon Guest',
       author_email='simon.guest@tesujimath.org',
       url='http://code.google.com/p/qlsync/',
       packages=['qlsync'],
-      scripts=['bin/qlsync'],
+      scripts=['bin/qlsync', 'bin/qlsync-playlist-from-device'],
       license='GPLv2'
      )
